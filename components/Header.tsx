cc0711c720dfdaf9ea59cf6e2543a72acd0df0c8
Luke <tofutree23@gmail.com>
2022-04-28 19:29:10 +0900

    add: Header with main logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,30 @@
+import Link from 'next/link'
+
 export default function Header() {
   return (
-    <div>
-      <h1>I am a Header</h1>
-    </div>
+    <header className="mx-auto flex max-w-7xl justify-between p-5">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-44 cursor-pointer object-contain"
+            src="/images/medium.png"
+            alt="main"
+          />
+        </Link>
+        <div className="hidden space-x-5 md:inline-flex">
+          <h3 className="px-4 py-1">About</h3>
+          <h3 className="px-4 py-1">Contact</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign in</h3>
+        <h3 className="rounded-full border border-green-600 px-4 py-1">
+          Get Started
+        </h3>
+      </div>
+    </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,37 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium rare blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
+
+      <div className="py10 flex items-center justify-between border-y border-black bg-yellow-400 lg:py-0">
+        <div className="space-y-5 px-10">
+          <h1 className="max-w-xl font-serif text-6xl">
+            <span className="underline decoration-black decoration-4">
+              Medium rare
+            </span>
+            is a place to write, read, and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h2>
+        </div>
+        <img
+          className="hidden h-32 md:inline-flex lg:h-full"
+          src="/images/medium-logo.png"
+          alt="logo"
+        />
+      </div>
+      {/* Posts */}
     </div>
   )
 }
